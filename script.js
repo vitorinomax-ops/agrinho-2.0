8772ffe447943365e60919a71ec72ea2e4e87ae0
MAX AUGUSTO DONDONI VITORINO <vitorino.max@escola.pr.gov.br>
2026-06-01 10:23:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -107,3 +107,115 @@ document
     window.speechSynthesis.cancel();
 
 });
+/* ==================================
+ACESSIBILIDADE
+================================== */
+
+const html = document.documentElement;
+
+let currentFont =
+parseInt(
+    localStorage.getItem("fontSize")
+) || 16;
+
+html.style.setProperty(
+    "--base-font-size",
+    currentFont + "px"
+);
+
+/* ==========================
+AUMENTAR FONTE
+========================== */
+
+document
+.getElementById("increaseFont")
+.addEventListener("click", () => {
+
+    if(currentFont < 26){
+
+        currentFont += 2;
+
+        html.style.setProperty(
+            "--base-font-size",
+            currentFont + "px"
+        );
+
+        localStorage.setItem(
+            "fontSize",
+            currentFont
+        );
+
+    }
+
+});
+
+/* ==========================
+DIMINUIR FONTE
+========================== */
+
+document
+.getElementById("decreaseFont")
+.addEventListener("click", () => {
+
+    if(currentFont > 12){
+
+        currentFont -= 2;
+
+        html.style.setProperty(
+            "--base-font-size",
+            currentFont + "px"
+        );
+
+        localStorage.setItem(
+            "fontSize",
+            currentFont
+        );
+
+    }
+
+});
+
+/* ==========================
+RESETAR FONTE
+========================== */
+
+document
+.getElementById("resetFont")
+.addEventListener("click", () => {
+
+    currentFont = 16;
+
+    html.style.setProperty(
+        "--base-font-size",
+        "16px"
+    );
+
+    localStorage.setItem(
+        "fontSize",
+        16
+    );
+
+});
+const body = document.body;
+
+if(localStorage.getItem("theme") === "dark"){
+    body.classList.add("dark-mode");
+}
+
+document
+.getElementById("toggleTheme")
+.addEventListener("click", () => {
+
+    body.classList.toggle("dark-mode");
+
+    const theme =
+    body.classList.contains("dark-mode")
+    ? "dark"
+    : "light";
+
+    localStorage.setItem(
+        "theme",
+        theme
+    );
+
+});
