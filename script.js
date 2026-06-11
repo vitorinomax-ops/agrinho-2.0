c13b500031ce354b557ad7c2dce8acaba0b8758d
MAX AUGUSTO DONDONI VITORINO <vitorino.max@escola.pr.gov.br>
2026-06-11 09:25:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -512,3 +512,25 @@ topButton.style.visibility = "hidden";
     );
 
 });
+const observer =
+new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+document
+.querySelectorAll(
+".card,.timeline-item,.imagem"
+)
+.forEach(el => observer.observe(el));
